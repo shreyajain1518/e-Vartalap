417a2777b8a4bd92334890b47b21aba802637bdf
aakhadmalashvini10@gmail.com <aakhadmalashvini10@gmail.com>
2019-01-21 18:01:33 +0530

    third change

--- e-vartalap/src/main/java/com/app/evartalap/evartalap/pojos/mysqlpojos/Role.java
+++ e-vartalap/src/main/java/com/app/evartalap/evartalap/pojos/mysqlpojos/Role.java
@@ -1,5 +1,38 @@
 package com.app.evartalap.evartalap.pojos.mysqlpojos;
 
+import javax.persistence.*;
+
+@Entity
 public class Role {
+	
+	int role_id;
+	String role_name;
+	public Role() {
+		super();
+	}
+	public Role(String role_name) {
+		super();
+		this.role_name = role_name;
+	}
+	
+	@Id  
+	@GeneratedValue(strategy=GenerationType.AUTO)  
+	public int getRole_id() {
+		return role_id;
+	}
+	public void setRole_id(int role_id) {
+		this.role_id = role_id;
+	}
+	public String getRole_name() {
+		return role_name;
+	}
+	public void setRole_name(String role_name) {
+		this.role_name = role_name;
+	}
+	@Override
+	public String toString() {
+		return "Role [role_id=" + role_id + ", role_name=" + role_name + "]";
+	}
+	
 
 }
--- e-vartalap/src/main/java/com/app/evartalap/evartalap/pojos/mysqlpojos/User.java
+++ e-vartalap/src/main/java/com/app/evartalap/evartalap/pojos/mysqlpojos/User.java
@@ -1,5 +1,89 @@
 package com.app.evartalap.evartalap.pojos.mysqlpojos;
+import javax.persistence.*;
+import com.mysql.cj.jdbc.Blob;
 
-public enum User {
+@Entity
+public class User {
 
+	int user_id;
+	String user_name;
+	String user_email;
+	String user_password;
+	Role role;
+	Blob user_photo;
+	
+	public User() {
+		super();
+	}
+
+	public User(String user_name, String user_email, String user_password, Role role, Blob user_photo) {
+		super();
+		this.user_name = user_name;
+		this.user_email = user_email;
+		this.user_password = user_password;
+		this.role = role;
+		this.user_photo = user_photo;
+	}
+    
+    @Id  
+    @GeneratedValue(strategy=GenerationType.AUTO)  
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	@Column(unique = true)
+	public String getUser_email() {
+		return user_email;
+	}
+
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+
+	public String getUser_password() {
+		return user_password;
+	}
+
+	public void setUser_password(String user_password) {
+		this.user_password = user_password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public Blob getUser_photo() {
+		return user_photo;
+	}
+
+	public void setUser_photo(Blob user_photo) {
+		this.user_photo = user_photo;
+	}
+
+	@Override
+	public String toString() {
+		return "User [user_id=" + user_id + ", user_name=" + user_name + ", user_email=" + user_email
+				+ ", user_password=" + user_password + ", role=" + role + ", user_photo=" + user_photo + "]";
+	}
+	
+	
+	
+	
+	
 }
