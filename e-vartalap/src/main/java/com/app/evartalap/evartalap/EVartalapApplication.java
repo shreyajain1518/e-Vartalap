cbaa484f90d9c3e683f293551eae2d64638841d5
shreyajain1518 <shreyajain1518@gmail.com>
2019-01-21 15:58:13 +0530

    first commit

---
+++ e-vartalap/src/main/java/com/app/evartalap/evartalap/EVartalapApplication.java
@@ -0,0 +1,14 @@
+package com.app.evartalap.evartalap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVartalapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EVartalapApplication.class, args);
+	}
+
+}
+
---
+++ e-vartalap/src/test/java/com/app/evartalap/evartalap/EVartalapApplicationTests.java
@@ -0,0 +1,17 @@
+package com.app.evartalap.evartalap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EVartalapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
