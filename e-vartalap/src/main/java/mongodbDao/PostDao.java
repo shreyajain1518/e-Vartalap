8a0a8c8c2520993731dfb4db2ccd1d215e19c00a
shreyajain1518 <shreyajain1518@gmail.com>
2019-01-21 18:45:00 +0530

    sixth change

--- e-vartalap/src/main/java/mongodbDao/PostDao.java
+++ e-vartalap/src/main/java/mongodbDao/PostDao.java
@@ -4,6 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.app.evartalap.evartalap.pojos.mongodbpojos.Post;
 
-public interface PostDao extends MongoRepository<Post,post_id>{
+public interface PostDao extends MongoRepository<Post,Integer>{
 
 }
